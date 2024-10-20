8471cdc95011eb937d2581a46fd48815e66d32a6
VanhoveHugo <vanhovehugo1@gmail.com>
2024-10-20 03:24:11 +0200

    Add Swagger, Refacto Config & Update .gitignore

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,26 @@
-export const PORT = process.env.PORT ?? 3000;
-export const JWT_SECRET = process.env.JWT_SECRET ?? "secret";
+export const config = {
+  PORT: process.env.PORT ?? 3000,
+  JWT_SECRET: process.env.JWT_SECRET ?? "secret",
+  NODE_ENV: process.env.NODE_ENV ?? "development",
+  SWAGGER_OPTIONS: {
+    swaggerDefinition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Ceticy",
+        version: "1.0.0",
+        description: "API documentation for Ceticy Application",
+        contact: {
+          name: "Hugo V.",
+          url: "https://vanhovehugo.dev",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+          description: "Development",
+        },
+      ],
+    },
+    apis: ["./src/routes/*.ts"],
+  },
+};
--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -2,12 +2,37 @@ import { Router } from "express";
 
 const router: Router = Router();
 
-// Welcome message
+/**
+ * @swagger
+ * tags:
+ *   - name: Debug
+ *     description: Initial routes
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     tags: [Debug]
+ *     summary: Welcome message
+ *     responses:
+ *       200:
+ *         description: Welcome to the API
+ */
 router.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
 
-// Try middleware Error
+/**
+ * @swagger
+ * /e:
+ *   get:
+ *     tags: [Debug]
+ *     summary: Test error handling
+ *     responses:
+ *       200:
+ *         description: Try middleware error handling
+ */
 router.get("/e", (req, res, next) => {
   try {
     throw new Error("Woops");
