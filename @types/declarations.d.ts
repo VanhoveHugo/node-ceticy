73b4e97bb58e89fb93badab87cc8abb14c091dc6
VanhoveHugo <vanhovehugo1@gmail.com>
2024-10-22 10:51:54 +0200

    Declarations Fix Codacy

---
+++ @types/declarations.d.ts
@@ -0,0 +1,13 @@
+declare module "cors";
+declare module "compression";
+declare module "swagger-jsdoc";
+declare module "swagger-ui-express";
+
+declare const Express: () => void;
+declare const dotenv: { config: () => void };
+declare const cors: () => void;
+declare const express: { urlencoded: () => void, json: () => void };
+declare const helmet: () => void;
+declare const swaggerJsDoc: () => void;
+declare const compression: () => void;
+declare const swaggerUi: { serve: void; setup: () => void };
